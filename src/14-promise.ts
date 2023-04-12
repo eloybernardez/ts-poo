11d466f5a7c75478013e1d27d011ab4121b670b7
eloy <eloy.ricardo.550@gmail.com>
2023-04-11 22:27:21 -0300

    [ADD] promises

---
+++ src/14-promise.ts
@@ -0,0 +1,34 @@
+import axios from "axios"
+
+(async () => {
+  function delay(time: number) {
+    const promise: Promise<boolean> = new Promise(resolve => {
+      setTimeout(() => {
+        resolve(true)
+      }, time)
+    })
+    return promise
+  }
+
+  function getProducts() {
+    const promise = axios.get('https://api.escuelajs.co/api/v1/products')
+    return promise
+  }
+
+  async function getProductsAsync() {
+    const rta = await axios.get('https://api.escuelajs.co/api/v1/products')
+    return rta
+  }
+
+
+  console.log('---'.repeat(10))
+  const rta: boolean = await delay(3000)
+  console.log(rta)
+
+  console.log('---'.repeat(10))
+  const products = await getProducts()
+  console.log(products.data)
+
+  const productsv2 = await getProductsAsync()
+  console.log(productsv2)
+})()
